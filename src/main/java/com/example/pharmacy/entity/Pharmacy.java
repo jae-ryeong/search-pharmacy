dd0cbe0d2915a7eb9db1c670a877b386e7e8d9af
김재령 <wofud0321@naver.com>
2023-12-05 14:12:33 +0900

    auditing 추가

--- src/main/java/com/example/pharmacy/entity/Pharmacy.java
+++ src/main/java/com/example/pharmacy/entity/Pharmacy.java
@@ -1,17 +1,21 @@
 package com.example.pharmacy.entity;
 
+import com.example.pharmacy.BaseTimeEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "pharmacy")
-public class Pharmacy {
+public class Pharmacy extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -20,4 +24,8 @@ public class Pharmacy {
     private String pharmacyAddress;
     private double latitude;
     private double longitude;
+
+    public void changePharmacyAddress(String address) {
+        this.pharmacyAddress = address;
+    }
 }
