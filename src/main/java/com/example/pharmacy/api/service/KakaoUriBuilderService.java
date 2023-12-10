537e45ca2a620e3448de00e16d5b400e1260745a
김재령 <wofud0321@naver.com>
2023-12-10 16:23:39 +0900

    category 검색 기능
    카카오에서 제공하는 약국뿐만이 아닌 카테고리로 검색하는 기능 구현

--- src/main/java/com/example/pharmacy/api/service/KakaoUriBuilderService.java
+++ src/main/java/com/example/pharmacy/api/service/KakaoUriBuilderService.java
@@ -13,6 +13,7 @@ import java.net.URI;
 public class KakaoUriBuilderService {
 
     private static final String KAKAO_LOCAL_SEARCH_ADDRESS_URL = "https://dapi.kakao.com/v2/local/search/address.json";
+    private static final String KAKAO_LOCAL_CATEGORY_SEARCH_URL = "https://dapi.kakao.com/v2/local/search/category.json";
 
     public URI buildUriByAddressSearch(String address) {    // Uri를 만들어주는 서비스코드
         UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(KAKAO_LOCAL_SEARCH_ADDRESS_URL);
@@ -20,6 +21,23 @@ public class KakaoUriBuilderService {
 
         URI uri = uriBuilder.build().encode().toUri();
         log.info("[KakaoUriBuilderService buildUriByAddressSearch] address: {}, uri: {}", address, uri);
+
+        return uri;
+    }
+
+    public URI buildUriByCategorySearch(double latitude, double longitude, double radius, String category){
+        double meterRadius = radius * 1000; // meter 단위
+
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(KAKAO_LOCAL_CATEGORY_SEARCH_URL);
+        uriBuilder.queryParam("category_group_codde", category);
+        uriBuilder.queryParam("x", longitude);
+        uriBuilder.queryParam("y", latitude);
+        uriBuilder.queryParam("radius", meterRadius);
+        uriBuilder.queryParam("sort", "distance");
+
+        URI uri = uriBuilder.build().encode().toUri();
+
+        log.info("[KakaoUriBuilderService buildUriByCategorySearch] uri: {}", uri);
 
         return uri;
     }
