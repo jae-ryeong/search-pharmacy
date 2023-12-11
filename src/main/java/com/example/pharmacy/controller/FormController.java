2f3e5a6eda9b83a08f9c2a2151b3a5a79cca25b5
김재령 <wofud0321@naver.com>
2023-12-11 21:14:05 +0900

    컨트롤러 및 html파일 구현

---
+++ src/main/java/com/example/pharmacy/controller/FormController.java
@@ -0,0 +1,32 @@
+package com.example.pharmacy.controller;
+
+import com.example.pharmacy.dto.InputDto;
+import com.example.pharmacy.service.PharmacyRecommendationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@RequiredArgsConstructor
+@Controller
+public class FormController {
+
+    private final PharmacyRecommendationService pharmacyRecommendationService;
+
+    @GetMapping("/")
+    public String main() {
+        return "main";
+    }
+
+    @PostMapping("/search")
+    public ModelAndView postDirection(@ModelAttribute InputDto inputDto) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("output");
+        modelAndView.addObject("outputFormList",
+                pharmacyRecommendationService.recommendPharmacyList(inputDto.address()));
+
+        return modelAndView;
+    }
+}
