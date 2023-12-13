94aa2cfea8aef46801a6f8f98499f729fdc534a6
김재령 <wofud0321@naver.com>
2023-12-13 17:59:20 +0900

    service코드를 controller에서 사용하도록 구현

--- src/main/java/com/example/pharmacy/service/PharmacyRecommendationService.java
+++ src/main/java/com/example/pharmacy/service/PharmacyRecommendationService.java
@@ -3,14 +3,17 @@ package com.example.pharmacy.service;
 import com.example.pharmacy.api.dto.DocumentDto;
 import com.example.pharmacy.api.dto.KakaoApiResponseDto;
 import com.example.pharmacy.api.service.KakaoAddressSearchService;
+import com.example.pharmacy.dto.OutputDto;
 import com.example.pharmacy.entity.Direction;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -21,20 +24,33 @@ public class PharmacyRecommendationService {
     private final DirectionService directionService;
 
     // 주소 입력 -> 위치 기반 데이터로 변환 -> validation 체크 -> 변환된 위도 경도 기준으로 가까운 약국 찾기
-    public void recommendPharmacyList(String address) {
+    public List<OutputDto> recommendPharmacyList(String address) {
 
         KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
 
         if (Objects.isNull(kakaoApiResponseDto) || CollectionUtils.isEmpty(kakaoApiResponseDto.documentList())) {
             log.error("[PharmacyRecommendationService recommendPharmacyList fail] Input address: {}", address);
-            return;
+            return Collections.emptyList();
         }
 
         DocumentDto documentDto = kakaoApiResponseDto.documentList().get(0);
 
         List<Direction> directionList = directionService.buildDirectionList(documentDto);   // 가까운 약국 리스트(순서대로)
-        List<Direction> categoryDirectionList = directionService.buildDirectionListByCategoryApi(documentDto);
+        //List<Direction> categoryDirectionList = directionService.buildDirectionListByCategoryApi(documentDto);
 
-        directionService.saveAll(directionList);
+        return directionService.saveAll(directionList)
+                .stream()
+                .map(t -> convertToOutputDto(t))
+                .collect(Collectors.toList());
+    }
+
+    public OutputDto convertToOutputDto(Direction direction) {
+        return OutputDto.builder()
+                .pharmacyName(direction.getTargetPharmacyName())
+                .pharmacyAddress(direction.getTargetAddress())
+                .directionUrl("todo")
+                .roadViewUrl("todo")
+                .distance(String.format("%.2f km", direction.getDistance()))
+                .build();
     }
 }
